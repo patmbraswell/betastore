964b9ba50859be64a23dbf8158a4ec6e2daf3b8c
Patrick Braswell <patmbraswell@gmail.com>
2013-10-07 19:34:59 -0400

    I think I messed this up

---
+++ betastore/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Betastore::Application.config.secret_key_base = '1f3cf9ff9d692fd36f81f1457f2c5d484049131ba3e5fc22821c71a0c2f14b4a698d85ac919ae91760ff5f2b61be8c85931bf530ad3abec127a006a7ba2f2cbe'
